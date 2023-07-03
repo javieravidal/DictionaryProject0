3bacccdb24c810e801c540ac8d6f4f21692e54ed
javieravidal <126521755+javieravidal@users.noreply.github.com>
2023-07-03 13:58:36 -0400

    Testing website

---
+++ dictionaryproject/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dictionary</h1>
+      <a href='#' className='btn btn-primary'>Hello</a>
+    </div>
+  );
+}
+
+export default App;
